9b1638657de1b8d5e37425a63470f8bbea9536ae
k.ishiwata <ki.ishiwata@gmail.com>
2024-05-03 22:58:00 +0900

    postsページネーション実装

--- posts/views.py
+++ posts/views.py
@@ -5,11 +5,13 @@ from .models import Post
 
 
 class IndexView(generic.ListView):
+    paginate_by = 10
+
     def get_queryset(self):
         return (
             Post.objects
                 .filter(is_public=True)
-                .order_by('-published_at')[:10]
+                .order_by('-published_at')
         )
 
 class DetailView(generic.DetailView):
